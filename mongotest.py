c0b4ccafc66b6390650dcddef6d98a69d885dc15
venkatesh <venkateshyadav.sonu@gmail.com>
2022-08-31 10:50:55 +0530

    this is my first code push in git

---
+++ mongotest.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+
+
+
+
+client = MongoClient("mongodb+srv://admin:admin@clusternameissame.txljzvs.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+d = {"name": "venkat", "age": 25}
+collection = db['test-collection']
+collection.insert_one(d)
+
